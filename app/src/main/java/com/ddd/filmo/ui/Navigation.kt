fc36f17968ce901af8057f04c5583f2cce546211
jungmal <jungmal@yonsei.ac.kr>
2023-07-24 23:34:20 +0900

    [feat] Navigation 에 MainScreen 추가 및 scaffold 삭제
    디자인 나온것들을 봤을 때 Top Bar 영역에 그라데이션이 들어가는 등 scaffold 로 묶기에 적합하지 않아보이므로 Top Bar 를 일단 화면별로 가져가는게 적합해 보입니다.

--- app/src/main/java/com/ddd/filmo/ui/Navigation.kt
+++ app/src/main/java/com/ddd/filmo/ui/Navigation.kt
@@ -17,6 +17,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.ddd.filmo.designsystem.component.appbar.FilmoTopAppBarState
+import com.ddd.filmo.main.MainScreen
 import com.ddd.filmo.presentation.login.ui.LoginScreen
 import com.ddd.filmo.presentation.mypage.ui.MyPageScreen
 
@@ -24,77 +25,21 @@ import com.ddd.filmo.presentation.mypage.ui.MyPageScreen
 @Composable
 fun MainNavigation() {
     val navController = rememberNavController()
-    val appState = rememberAppState()
-    var filmoTopAppBarState by remember {
-        mutableStateOf(
-            FilmoTopAppBarState(
-                "main",
-                {
-                    Text("action")
-                },
-                {
-                    Text("action")
-                },
-            ),
-        )
-    }
 
-    Scaffold(
-        topBar = {
-            if (!filmoTopAppBarState.isAllEmpty) {
-                TopAppBar(
-                    title = { filmoTopAppBarState.title?.let { Text(it) } },
-                    navigationIcon = { filmoTopAppBarState.onNavigation?.invoke() },
-                    actions = { filmoTopAppBarState.onAction?.invoke() },
-                    modifier = Modifier.padding(start = 16.dp, end = 16.dp),
-                )
-            }
-        },
-        bottomBar = {
-        },
-    ) { padding ->
+    Scaffold() { padding ->
         NavHost(
             navController = navController,
             startDestination = "main",
             modifier = Modifier.padding(padding),
         ) {
             composable("main") {
-                filmoTopAppBarState = FilmoTopAppBarState(
-                    "main",
-                    {
-                        Text("action")
-                    },
-                    {
-                        Text("action")
-                    },
-                )
-                LoginScreen(
-                    modifier = Modifier.padding(16.dp),
-//                    navigate = {
-//                        navController.navigate(it)
-//                    }
-                )
+                MainScreen()
             }
             composable("mypage") {
                 var count by remember {
                     mutableStateOf(0)
                 }
 
-                filmoTopAppBarState = FilmoTopAppBarState(
-                    "mypage",
-                    {
-                        Text(
-                            "back",
-                            modifier = Modifier.clickable {
-                                navController.navigateUp()
-                            },
-                        )
-                    },
-                    {
-                        Text("action")
-                        count++
-                    },
-                )
                 MyPageScreen(
                     count = count,
                 )
