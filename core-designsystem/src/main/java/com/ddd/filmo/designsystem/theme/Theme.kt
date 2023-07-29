8649f89977646506a5bb3da65dbcc93437601767
jungmal <jungmal@yonsei.ac.kr>
2023-07-29 15:44:23 +0900

    [design] statusbar 색상울 theme.xml 에서 가져오도록 수정

--- core-designsystem/src/main/java/com/ddd/filmo/designsystem/theme/Theme.kt
+++ core-designsystem/src/main/java/com/ddd/filmo/designsystem/theme/Theme.kt
@@ -26,10 +26,12 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
@@ -72,8 +74,10 @@ fun FilmoTheme(
     val view = LocalView.current
     if (!view.isInEditMode) {
         SideEffect {
-            (view.context as Activity).window.statusBarColor = colorScheme.primary.toArgb()
-            ViewCompat.getWindowInsetsController(view)?.isAppearanceLightStatusBars = darkTheme
+            /* for use themes.xml
+            val window = (view.context as Activity).window
+            window.statusBarColor = Color(0xff202020).toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme*/
         }
     }
 
