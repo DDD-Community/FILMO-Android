7cd498a2eb3c363525c46d231ef02f8f72e98aa1
jungmal <jungmal@yonsei.ac.kr>
2023-07-29 17:02:15 +0900

    [design] SceneReadScreen 수정

--- feature-scene/presentation/src/main/java/com/ddd/filmo/presentation/scene/ui/read/SceneReadScreen.kt
+++ feature-scene/presentation/src/main/java/com/ddd/filmo/presentation/scene/ui/read/SceneReadScreen.kt
@@ -9,7 +9,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -22,6 +24,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
+import com.ddd.filmo.designsystem.theme.FilmoColor
 import com.ddd.filmo.designsystem.theme.FilmoFamily
 import com.ddd.filmo.model.Scene
 
@@ -38,7 +41,9 @@ internal fun SceneReadScreenRoute(
 @Composable
 fun SceneReadScreen(scene: Scene) {
     Column(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState()),
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
         Row(
@@ -84,36 +89,52 @@ fun SceneReadScreen(scene: Scene) {
                 Spacer(modifier = Modifier.size(9.dp))
                 Row() {
                     for (i in 0 until scene.sceneRate!!) {
-                        Text(text = "★", color = Color(0xffFFE144))
+                        Text(text = "★", color = FilmoColor.PrimaryVariant)
                     }
                 }
             }
         }
 
-        Spacer(modifier = Modifier.size(30.dp))
+        Spacer(modifier = Modifier.size(32.dp))
+        
+        Column(Modifier.padding(horizontal = 16.dp)) {
+            AsyncImage(
+                model = scene.sceneImageUrl,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clip(RoundedCornerShape(13.dp))
+                    .aspectRatio(328f / 204f),
+                contentDescription = "",
+                contentScale = ContentScale.Crop,
+            )
 
-        AsyncImage(
-            model = scene.sceneImageUrl,
-            modifier = Modifier
-                .fillMaxWidth()
-                .clip(RoundedCornerShape(13.dp))
-                .aspectRatio(328f / 194f),
-            contentDescription = "",
-            contentScale = ContentScale.Crop,
-        )
+            Spacer(modifier = Modifier.size(24.dp))
 
-        Spacer(modifier = Modifier.size(30.dp))
+            Text(
+                text = scene.sceneText.toString(),
+                style = TextStyle(
+                    fontSize = 16.sp,
+                    lineHeight = 22.4.sp,
+                    fontFamily = FilmoFamily,
+                    fontWeight = FontWeight(400),
+                    color = Color(0xFFF4F4F4),
+                ),
+                modifier = Modifier.align(Alignment.Start),
+            )
 
-        Text(
-            text = scene.sceneText.toString(),
-            style = TextStyle(
-                fontSize = 14.sp,
-                lineHeight = 20.sp,
-                fontWeight = FontWeight(400),
-                color = Color(0xFF332D41),
-                letterSpacing = 0.14.sp,
-            ),
-            modifier = Modifier.align(Alignment.Start),
-        )
+            Spacer(modifier = Modifier.size(24.dp))
+
+            Text(
+                text = scene.createdAtString,
+                style = TextStyle(
+                    fontSize = 14.sp,
+                    lineHeight = 19.6.sp,
+                    fontFamily = FilmoFamily,
+                    fontWeight = FontWeight(400),
+                    color = Color(0xFFB6B6B6),
+                    letterSpacing = 0.14.sp,
+                )
+            )
+        }
     }
 }
