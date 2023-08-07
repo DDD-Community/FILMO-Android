9b8c6bc172babed123db242c67ee2367e5946c31
최우성 <63629853+choi-woo-sung@users.noreply.github.com>
2023-08-07 18:40:18 +0900

    [feat] Setting 화면 개발진행중

--- core-designsystem/src/main/java/com/ddd/filmo/designsystem/component/appbar/FilmoAppBar.kt
+++ core-designsystem/src/main/java/com/ddd/filmo/designsystem/component/appbar/FilmoAppBar.kt
@@ -2,21 +2,38 @@ package com.ddd.filmo.designsystem.component.appbar
 
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 import com.ddd.filmo.designsystem.theme.FilmoColor
+import com.ddd.filmo.designsystem.theme.FilmoFamily
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FilmoAppBar(
     actions: @Composable (RowScope.() -> Unit),
     navigationIcon: @Composable () -> Unit,
+    title: String = "",
 ) {
     TopAppBar(
         actions = actions,
         navigationIcon = navigationIcon,
-        title = {},
+        title = {
+            Text(
+                text = title,
+                style = TextStyle(
+                    fontSize = 20.sp,
+                    lineHeight = 28.sp,
+                    fontFamily = FilmoFamily,
+                    fontWeight = FontWeight(500),
+                    color = FilmoColor.txt_01,
+                )
+            )
+        },
         colors = TopAppBarDefaults.topAppBarColors(
             containerColor = FilmoColor.Background,
             actionIconContentColor = FilmoColor.ic_01,
---
+++ feature-setting/presentation/src/main/java/com/ddd/filmo/presentation/setting/SettingScreen.kt
@@ -0,0 +1,122 @@
+/*
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.ddd.filmo.presentation.setting
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.ddd.filmo.designsystem.component.appbar.FilmoAppBar
+import com.ddd.filmo.designsystem.component.button.FilmoLoginButton
+import com.ddd.filmo.designsystem.icon.FilmoIcon
+import com.ddd.filmo.designsystem.theme.FilmoColor
+import com.ddd.filmo.designsystem.theme.FilmoFamily
+import com.ddd.filmo.designsystem.theme.FilmoTheme
+
+@Composable
+fun SettingScreenRoute(navigateToMain: () -> Unit) {
+    SettingScreen(navigateToMain)
+}
+
+@Composable
+internal fun SettingScreen(loginButtonClicked: () -> Unit = {}) {
+    Column(
+        Modifier.fillMaxSize().background(FilmoColor.Background),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = androidx.compose.ui.Alignment.CenterHorizontally,
+    ) {
+        FilmoAppBar(
+            actions = {},
+            navigationIcon = {
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(painter = painterResource(id = FilmoIcon.Back), contentDescription = "")
+                }
+            },
+            title = "설정",
+        )
+        var nameLogin by remember { mutableStateOf("Compose") }
+        Image(painter = painterResource(id = FilmoIcon.FIlmoTextLogo), contentDescription = "")
+        Spacer(modifier = Modifier.height(6.dp))
+        Text(
+            text = "나만의 필름 모아",
+            style = TextStyle(
+                fontSize = 20.sp,
+                lineHeight = 30.sp,
+                fontFamily = FilmoFamily,
+                fontWeight = FontWeight(500),
+                color = FilmoColor.txt_05,
+                textAlign = TextAlign.Center,
+            ),
+        )
+        Spacer(modifier = Modifier.height(36.dp))
+        FilmoLoginButton(
+            onClick = loginButtonClicked,
+            text = "카카오로 시작하기",
+            drawble = FilmoIcon.Kakao,
+            containsColor = Color(0xFFFEE500),
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+        FilmoLoginButton(
+
+            onClick = loginButtonClicked,
+            text = "구글로 시작하기",
+            drawble = FilmoIcon.Google,
+            containsColor = Color.White,
+        )
+    }
+}
+
+@Composable
+fun a() {
+//
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun DefaultLoginPreview() {
+    FilmoTheme {
+        SettingScreen()
+    }
+}
+
+@Preview(showBackground = true, widthDp = 480)
+@Composable
+private fun PortraitLoginPreview() {
+    FilmoTheme {
+        SettingScreen()
+    }
+}
