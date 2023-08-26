2c6fe29d87a070645bf93d9a7a0ec5e93279650d
jungmal <jungmal@yonsei.ac.kr>
2023-08-26 16:16:45 +0900

    [feat] SceneDetailScreen 의 라우팅 수정

--- feature-scene/presentation/src/main/java/com/ddd/filmo/presentation/scene/ui/detail/SceneDetailScreen.kt
+++ feature-scene/presentation/src/main/java/com/ddd/filmo/presentation/scene/ui/detail/SceneDetailScreen.kt
@@ -26,6 +26,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -59,150 +60,22 @@ fun SceneDetailScreen(
     scene: Scene? = Scene.mock,
     onBackClick: () -> Unit = {},
 ) {
-    var isDropDownMenuExpanded by remember { mutableStateOf(false) }
     var screenIndex by remember { mutableStateOf(0) }
-    var selectedFilm by remember { mutableStateOf<Film?>(null) }
 
-    val selectFilm = fun (film: Film) {
-        selectedFilm = film
+    val toEditScreen = fun() {
+        screenIndex = 1
     }
 
-    Column(Modifier.fillMaxSize()) {
-        Row(
-            Modifier
-                .padding(10.dp)
-                .fillMaxWidth()) {
-            IconButton(onClick = {
-                onBackClick()
-            }, modifier = Modifier.size(40.dp)) {
-                Icon(
-                    painter = painterResource(id = R.drawable.ic_arrow_back),
-                    contentDescription = "",
-                    modifier = Modifier.size(24.dp),
-                )
-            }
-            Spacer(modifier = Modifier.weight(1f))
-            IconButton(
-                onClick = {
-                    isDropDownMenuExpanded = true
-                },
-                modifier = Modifier.size(40.dp),
-            ) {
-                Icon(
-                    painter = painterResource(id = R.drawable.ic_three_dots_horizontal),
-                    contentDescription = "",
-                    modifier = Modifier.size(24.dp),
-                )
-                DropdownMenu(
-                    expanded = isDropDownMenuExpanded,
-                    onDismissRequest = {
-                        isDropDownMenuExpanded = false
-                    },
-                ) {
-                    // adding items
-                    listOf("씬 수정하기", "씬 삭제하기").forEachIndexed { itemIndex, itemValue ->
-                        DropdownMenuItem(
-                            onClick = {
-                                isDropDownMenuExpanded = false
-                                screenIndex = 1
-                            },
-                            text = { Text(text = itemValue) },
-                        )
-                    }
-                }
-            }
-        }
-
-        scene?.let {
-            if (screenIndex == 0) SceneReadScreen(scene) else SceneEditScreen(scene)
-        }
+    val toReadScreen = fun() {
+        screenIndex = 0
     }
 
-    ModalBottomSheet(
-        onDismissRequest = {},
-        modifier = Modifier.fillMaxWidth(),
-        sheetState = rememberModalBottomSheetState(),
-        shape = BottomSheetDefaults.ExpandedShape,
-        containerColor = BottomSheetDefaults.ContainerColor,
-    ) {
-        Column {
-            Row(
-                modifier = Modifier.padding(horizontal = 16.dp),
-                verticalAlignment = androidx.compose.ui.Alignment.CenterVertically,
-            ) {
-                Text(
-                    text = "필름 선택하기",
-                    style = TextStyle(
-                        fontSize = 20.sp,
-                        lineHeight = 28.sp,
-                        fontFamily = FilmoFamily,
-                        fontWeight = FontWeight(500),
-                        color = Color(0xFFF4F4F4),
-                    ),
-                    modifier = Modifier.weight(1f)
-                )
-                IconButton(
-                    onClick = { },
-                    modifier = Modifier.size(40.dp),
-                ) {
-                    Icon(
-                        painter = painterResource(FilmoIcon.X),
-                        contentDescription = "",
-                        modifier = Modifier.size(14.dp),
-                    )
-                }
-            }
-
-            Spacer(modifier = Modifier.height(16.dp))
-
-            for (film in Film.fakeFilmList) {
-                FilmListItem(film, selectedFilm, selectFilm)
-            }
+    scene?.let {
+        if (screenIndex == 0) {
+            SceneReadScreen(scene, toEditScreen)
+        } else {
+            SceneEditScreen(scene, toReadScreen)
         }
     }
 }
 
-@Composable
-fun FilmListItem(film: Film, selectedFilm: Film?, selectFilm: (Film) -> Unit = {}) {
-    val selected = (film == selectedFilm)
-    val borderOrNot =
-        if (selected) Modifier.border(width = 2.dp, color = Color(0xFFFFFFFF), shape = RoundedCornerShape(size = 8.dp))
-        else Modifier
-
-    Row(
-        modifier = Modifier
-            .padding(horizontal = 16.dp, vertical = 2.dp)
-            .then(borderOrNot)
-            .clickable {
-                selectFilm(film)
-            }
-            .padding(16.dp)
-            .fillMaxWidth(),
-        verticalAlignment = androidx.compose.ui.Alignment.CenterVertically,
-    ) {
-        Spacer(modifier = Modifier
-            .clip(RoundedCornerShape(4.dp))
-            .size(32.dp)
-            .background(Color(film.caseColor)))
-        Spacer(modifier = Modifier.width(12.dp))
-        Text(
-            text = "${film.name}",
-            style = TextStyle(
-                fontSize = 14.sp,
-                lineHeight = 22.sp,
-                fontFamily = FilmoFamily,
-                fontWeight = FontWeight(500),
-                color = Color(0xFFF4F4F4),
-                letterSpacing = 0.14.sp,
-            ),
-            modifier = Modifier.weight(1f)
-        )
-        if(selected) {
-            Icon(
-                painter = painterResource(FilmoIcon.Check),
-                contentDescription = "",
-                modifier = Modifier.size(32.dp),
-            )
-        }
-    }
-}
--- feature-scene/presentation/src/main/java/com/ddd/filmo/presentation/scene/ui/edit/SceneEditScreen.kt
+++ feature-scene/presentation/src/main/java/com/ddd/filmo/presentation/scene/ui/edit/SceneEditScreen.kt
@@ -1,12 +1,14 @@
 package com.ddd.filmo.presentation.scene.ui.edit
 
 import android.net.Uri
+import androidx.activity.compose.BackHandler
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -21,7 +23,9 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.BottomSheetDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -39,12 +43,19 @@ import com.ddd.filmo.designsystem.theme.FilmoFamily
 import com.ddd.filmo.model.Scene
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.TextField
+import androidx.compose.material3.rememberModalBottomSheetState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.composed
 import androidx.compose.ui.draw.drawWithCache
 import androidx.compose.ui.geometry.CornerRadius
@@ -54,13 +65,16 @@ import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.Dp
+import com.ddd.filmo.core.designsystem.R
 import com.ddd.filmo.designsystem.icon.FilmoIcon
 import com.ddd.filmo.model.Film
 
-
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun SceneEditScreen(scene: Scene) {
+fun SceneEditScreen(
+    scene: Scene,
+    toReadScreen: () -> Unit
+) {
     val selectedFilm = remember { mutableStateOf(Film.fakeFilm0) }
     val selectedUri = remember { mutableStateOf<Uri?>(null) }
     val sceneText = remember { mutableStateOf("A poor yet passionate young man falls in love with a rich young woman, giving her a sense of freedom. However, social differences soon get in the way. \n\n A poor yet passionate young man falls in love with a rich young woman, giving her a sense of freedom. However, social differences soon get in the way.")}
@@ -69,9 +83,56 @@ fun SceneEditScreen(scene: Scene) {
         onResult = { uri -> selectedUri.value = uri }
     )
 
+    var isSheetOpen by rememberSaveable {
+        mutableStateOf(false)
+    }
+
+    val selectFilm = fun (film: Film) {
+        selectedFilm.value = film
+    }
+
+    val closeSheet = fun () {
+        isSheetOpen = false
+    }
+
+    BackHandler {
+        if (isSheetOpen) isSheetOpen = false
+        else toReadScreen()
+    }
+
     Column(
         Modifier.fillMaxSize()
     ) {
+        Row(
+            Modifier
+                .padding(10.dp)
+                .fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            IconButton(onClick = {
+                toReadScreen()
+            }, modifier = Modifier.size(48.dp)) {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_arrow_back),
+                    contentDescription = "",
+                    modifier = Modifier.size(24.dp),
+                )
+            }
+            
+            Spacer(modifier = Modifier.width(10.dp))
+
+            Text(
+                text = "씬 수정하기",
+                style = TextStyle(
+                    fontSize = 20.sp,
+                    lineHeight = 28.sp,
+                    fontFamily = FilmoFamily,
+                    fontWeight = FontWeight(500),
+                    color = Color(0xFFF4F4F4),
+                )
+            )
+        }
+
         Column(
             modifier = Modifier
                 .weight(1f)
@@ -199,13 +260,21 @@ fun SceneEditScreen(scene: Scene) {
 
             Spacer(modifier = Modifier.height(24.dp))
 
-            TextField(
+            BasicTextField(
                 value = "${sceneText.value}",
                 onValueChange = { sceneText.value = it },
                 modifier = Modifier
                     .fillMaxWidth()
                     .defaultMinSize(minHeight = 294.dp)
                     .background(color = Color(0xFF393939), shape = RoundedCornerShape(size = 8.dp))
+                    .padding(16.dp),
+                textStyle = TextStyle(
+                    fontSize = 16.sp,
+                    lineHeight = 22.4.sp,
+                    fontFamily = FilmoFamily,
+                    fontWeight = FontWeight(400),
+                    color = Color(0xFFF4F4F4),
+                )
             )
 
             Spacer(modifier = Modifier.height(24.dp))
@@ -213,7 +282,9 @@ fun SceneEditScreen(scene: Scene) {
             Row(
                 modifier = Modifier
                     .clip(RoundedCornerShape(size = 8.dp))
-                    .clickable { selectedFilm.value = Film.fakeFilm1 }
+                    .clickable {
+                        isSheetOpen = true
+                    }
                     .border(
                         width = 1.dp,
                         color = Color(0xFF7D7A7A),
@@ -274,6 +345,63 @@ fun SceneEditScreen(scene: Scene) {
 
     }
 
+    if (isSheetOpen) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color(0x40000000)),
+        ) {
+            Spacer(modifier = Modifier
+                .fillMaxSize()
+                .noRippleClickable { isSheetOpen = false })
+            Column(
+                modifier = Modifier
+                    .padding(top = 20.dp)
+                    .clip(shape = RoundedCornerShape(topStart = 24.dp, topEnd = 24.dp))
+//                    .fillMaxSize()
+                    .background(Color(0xFF393939))
+                    .align(Alignment.BottomCenter)
+            ) {
+                Spacer(modifier = Modifier.size(22.dp))
+                Row(
+                    modifier = Modifier.padding(horizontal = 16.dp),
+                    verticalAlignment = Alignment.CenterVertically,
+                ) {
+                    Text(
+                        text = "필름 선택하기",
+                        style = TextStyle(
+                            fontSize = 20.sp,
+                            lineHeight = 28.sp,
+                            fontFamily = FilmoFamily,
+                            fontWeight = FontWeight(500),
+                            color = Color(0xFFF4F4F4),
+                        ),
+                        modifier = Modifier.weight(1f)
+                    )
+                    IconButton(
+                        onClick = {
+                            isSheetOpen = false
+                        },
+                        modifier = Modifier.size(40.dp),
+                    ) {
+                        Icon(
+                            painter = painterResource(FilmoIcon.X),
+                            contentDescription = "",
+                            modifier = Modifier.size(14.dp),
+                        )
+                    }
+                }
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                for (film in Film.fakeFilmList) {
+                    FilmListItem(film, selectedFilm.value, selectFilm, closeSheet)
+                }
+
+                Spacer(modifier = Modifier.height(24.dp))
+            }
+        }
+    }
 }
 
 fun Modifier.dashedBorder(strokeWidth: Dp, color: Color, cornerRadiusDp: Dp) = composed(
@@ -300,3 +428,57 @@ fun Modifier.dashedBorder(strokeWidth: Dp, color: Color, cornerRadiusDp: Dp) = c
         )
     }
 )
+
+fun Modifier.noRippleClickable(onClick: ()->Unit): Modifier = composed {
+    clickable(indication = null,
+        interactionSource = remember { MutableInteractionSource() }) {
+        onClick()
+    }
+}
+
+
+@Composable
+fun FilmListItem(film: Film, selectedFilm: Film?, selectFilm: (Film) -> Unit = {}, closeSheet: () -> Unit) {
+    val selected = (film == selectedFilm)
+    val borderOrNot =
+        if (selected) Modifier.border(width = 2.dp, color = Color(0xFFFFFFFF), shape = RoundedCornerShape(size = 8.dp))
+        else Modifier
+
+    Row(
+        modifier = Modifier
+            .padding(horizontal = 16.dp, vertical = 2.dp)
+            .then(borderOrNot)
+            .clickable {
+                selectFilm(film)
+                closeSheet()
+            }
+            .padding(16.dp)
+            .fillMaxWidth(),
+        verticalAlignment = androidx.compose.ui.Alignment.CenterVertically,
+    ) {
+        Spacer(modifier = Modifier
+            .clip(RoundedCornerShape(4.dp))
+            .size(32.dp)
+            .background(Color(film.caseColor)))
+        Spacer(modifier = Modifier.width(12.dp))
+        Text(
+            text = "${film.name}",
+            style = TextStyle(
+                fontSize = 14.sp,
+                lineHeight = 22.sp,
+                fontFamily = FilmoFamily,
+                fontWeight = FontWeight(500),
+                color = Color(0xFFF4F4F4),
+                letterSpacing = 0.14.sp,
+            ),
+            modifier = Modifier.weight(1f)
+        )
+        if(selected) {
+            Icon(
+                painter = painterResource(FilmoIcon.Check),
+                contentDescription = "",
+                modifier = Modifier.size(32.dp),
+            )
+        }
+    }
+}
