2cc55dc9590f2017ac01c1dcb67c0849638ee725
jungmal <jungmal@yonsei.ac.kr>
2023-09-04 15:58:30 +0900

    [feat]FilmRepository가 filmRemoteDataSource의 films 를 가져오도록 수정

--- feature-film/domain/src/main/java/com/ddd/filmo/film/domain/repository/FilmRepository.kt
+++ feature-film/domain/src/main/java/com/ddd/filmo/film/domain/repository/FilmRepository.kt
@@ -1,4 +1,8 @@
 package com.ddd.filmo.film.domain.repository
 
+import com.ddd.filmo.model.Film
+import kotlinx.coroutines.flow.StateFlow
+
 interface FilmRepository {
+    val films: StateFlow<List<Film>>
 }
