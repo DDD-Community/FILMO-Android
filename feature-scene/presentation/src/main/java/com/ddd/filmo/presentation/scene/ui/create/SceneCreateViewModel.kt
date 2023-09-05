b4d44b2025b4e53ba29505cb2534e0d06dc83760
jungmal <jungmal@yonsei.ac.kr>
2023-09-05 22:57:33 +0900

    [feat] SceneCreateScreen, ViewModel 추가

---
+++ feature-scene/presentation/src/main/java/com/ddd/filmo/presentation/scene/ui/create/SceneCreateViewModel.kt
@@ -0,0 +1,40 @@
+package com.ddd.filmo.presentation.scene.ui.create
+
+import android.net.Uri
+import androidx.lifecycle.ViewModel
+import com.ddd.filmo.film.domain.repository.FilmRepository
+import com.ddd.filmo.login.domain.repository.UserRepository
+import com.ddd.filmo.model.Film
+import com.ddd.filmo.model.Scene
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import javax.inject.Inject
+
+@HiltViewModel
+class SceneCreateViewModel  @Inject constructor(
+    private val userRepository: UserRepository,
+    private val filmRepository: FilmRepository,
+) : ViewModel() {
+    val scene = MutableStateFlow(Scene())
+    val films = filmRepository.films.value
+    val sceneText = MutableStateFlow("")
+    val movieTitle = MutableStateFlow("")
+    val selectedFilm = MutableStateFlow(filmRepository.films.value.first())
+    val selectedUri: MutableStateFlow<Uri?> = MutableStateFlow(null)
+
+    fun setSceneText(value: String) {
+        sceneText.value = value
+    }
+
+    fun setMovieTitle(value: String) {
+        movieTitle.value = value
+    }
+
+    fun setSelectedUri(value: Uri?) {
+        selectedUri.value = value
+    }
+
+    fun selectFilm (film: Film) {
+        selectedFilm.value = film
+    }
+}
